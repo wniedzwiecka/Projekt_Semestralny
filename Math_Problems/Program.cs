48d926671272a7566c309734e5d7c435e79d01c0
Wiktoria Niedźwiecka <w.niedzwiecka54@gmail.com>
2020-07-06 22:11:21 +0200

    initial commit

---
+++ Math_Problems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Math_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeView = "\tMath Problems\n------------------------------\n1. START\n2. LEVELS\n3. RESTART\n4. EXIT\nPICK ACTION: ";
+            Console.WriteLine(welcomeView);
+
+            string given = Console.ReadLine();
+
+
+        }
+    }
+}
