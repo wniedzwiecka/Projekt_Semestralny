faabb3686cc9ef33a876dbd654bc5076ec8abdce
Wiktoria Niedźwiecka <w.niedzwiecka54@gmail.com>
2020-07-07 23:48:56 +0200

    rozpatrywanie wyboru użytkownika

--- Math_Problems/Program.cs
+++ Math_Problems/Program.cs
@@ -9,7 +9,30 @@ namespace Math_Problems
             string welcomeView = "\tMath Problems\n------------------------------\n1. START\n2. LEVELS\n3. RESTART\n4. EXIT\nPICK ACTION: ";
             Console.WriteLine(welcomeView);
 
-            string given = Console.ReadLine();
+            string userOption = Console.ReadLine();
+
+
+            if (userOption.Equals("1"))
+            {
+                Console.WriteLine("Wybrałeś start");
+            }
+            else if(userOption.Equals("2"))
+            {
+                Console.WriteLine("Wybrałeś drugą opcję");
+            }
+            else if(userOption.Equals("3"))
+            {
+                Console.WriteLine("Wybrałeś trzecią opcję");
+            }
+            else if (userOption.Equals("4"))
+            {
+                Console.WriteLine("Wybrałeś czwartą opcję");
+            }
+            else
+            {
+                Console.WriteLine("Wybrałeś złą opcję, proszę wybrać numery 1-4");
+            }
+          
 
 
         }
